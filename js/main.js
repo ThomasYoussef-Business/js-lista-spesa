8e71cbefff54d9804781396c8a21cd7b510f157a
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-15 17:36:11 +0100

    Fatto in modo che la lista si popoli con JavaScript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 const listaSpesa = ["Pomodori", "Biscotti", "Nutella", "Pesche", "Banane"];
+const contenitoreOggetti = document.getElementById("contenitore-lista");
 
 /**
  * let i = 0;
@@ -12,4 +13,15 @@ const listaSpesa = ["Pomodori", "Biscotti", "Nutella", "Pesche", "Banane"];
 for (let i = 0; i < listaSpesa.length; i++) {
     const oggettoDaComprare = listaSpesa[i];
     console.log(`Devo prendere ${oggettoDaComprare}`);
+    contenitoreOggetti.innerHTML += nuovoOggettoSpesa(oggettoDaComprare);
+}
+
+
+
+function rimuoviDaSpesa(elemento) {
+    console.log(elemento.parentNode);
+}
+
+function nuovoOggettoSpesa(nomeOggetto) {
+    return `<li class="list-group-item list-group-item-primary d-flex justify-content-between"><p class="m-0">${nomeOggetto}</p> <button type="button" onclick="rimuoviDaSpesa(this)" class="btn btn-danger"><span class="material-symbols-rounded">close</span></button></li>`;
 }
