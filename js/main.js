2c1c422e0bee9d7f65bb393f4b4f3125f771e364
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-15 20:58:10 +0100

    Fatto un po' di refactoring

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+/* ----------------------------- Variabili varie ---------------------------- */
 const listaSpesa = ["Pomodori", "Biscotti", "Nutella", "Pesche", "Banane"];
 const contenitoreOggetti = document.getElementById("contenitore-lista");
 const inputOggettoNuovo = document.getElementById("inputSpesa");
@@ -11,14 +12,16 @@ const inputOggettoNuovo = document.getElementById("inputSpesa");
  * }
  */
 
-for (let i = 0; i < listaSpesa.length; i++) {
-    const oggettoDaComprare = listaSpesa[i];
+for (const oggettoDaComprare of listaSpesa) {
     console.log(`Devo prendere ${oggettoDaComprare}`);
     contenitoreOggetti.innerHTML += oggettoSpesaComponente(oggettoDaComprare);
 }
 
+
 function aggiungiOggetto() {
-    if (inputOggettoNuovo.value) {
+    // Usato trim per evitare che whitespace siano abbastanza
+    // per aggiungere un nuovo oggetto
+    if (inputOggettoNuovo.value.trim()) {
         const nuovoOggetto = inputOggettoNuovo.value;
         inputOggettoNuovo.value = '';
         listaSpesa.push(nuovoOggetto);
@@ -26,19 +29,22 @@ function aggiungiOggetto() {
     }
 }
 
+
 function rimuoviDaSpesa(elemento) {
+
     const oggettoSpesa = elemento.parentNode;
     const indiceOggettoSpesa = prendiIndiceNodo(oggettoSpesa);
     const nomeOggetto = oggettoSpesa.firstChild.innerHTML;
-    console.log(`${indiceOggettoSpesa}, ${nomeOggetto}, ${listaSpesa}`);
+
     if (listaSpesa[indiceOggettoSpesa] == nomeOggetto) {
         listaSpesa.splice(indiceOggettoSpesa, 1)
         oggettoSpesa.remove();
     }
-    console.log(`${indiceOggettoSpesa}, ${nomeOggetto}, ${listaSpesa}`);
 }
 
 inputOggettoNuovo.addEventListener("keypress", function(event) {
+    // Premere invio sull'input causa l'avvio di un evento inteso per i form,
+    // quindi lo intercetto per cambiarne il comportamento
     if (event.key === "Enter") {
         event.preventDefault();
         aggiungiOggetto();
@@ -46,9 +52,13 @@ inputOggettoNuovo.addEventListener("keypress", function(event) {
 })
 
 function prendiIndiceNodo(elemento) {
+    // Soluzione un po' artigianale, non molto elegante ma funzionante
+    // TODO: Usare un oggetto per stare dietro agli elementi nella lista e
+    // per rimuoverli 
     return Array.prototype.indexOf.call(elemento.parentNode.children, elemento)
 }
 
+/* --------- Componente astratto a funzione per evitare ripetizione --------- */
 function oggettoSpesaComponente(nomeOggetto) {
     return `<li class="list-group-item list-group-item-primary d-flex justify-content-between"><p class="m-0">${nomeOggetto}</p> <button type="button" onclick="rimuoviDaSpesa(this)" class="btn btn-danger d-flex align-items-center"><span class="material-symbols-rounded">close</span></button></li>`;
 }
